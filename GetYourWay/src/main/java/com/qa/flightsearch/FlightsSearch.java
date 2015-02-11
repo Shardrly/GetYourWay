65ee4c73273e81bd3a294a17d8fb9f91a695e4ff
john@student <john@student.email>
2015-02-11 16:56:42 +0000

    Updates and search page work

--- GetYourWay/src/main/java/com/qa/flightsearch/FlightsSearch.java
+++ GetYourWay/src/main/java/com/qa/flightsearch/FlightsSearch.java
@@ -1,5 +1,15 @@
 package com.qa.flightsearch;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
+import com.qa.web.ScheduledFlights;
+import com.qa.web.TimeTest;
+
 public class FlightsSearch {
 	
 	   public static void main(String args[]) {
@@ -7,17 +17,76 @@ public class FlightsSearch {
 			
 //		   curl -v  -X GET "https://api.flightstats.com/flex/schedules/rest/v1/json/from/LHR/to/CDG/arriving/2015/2/13?appId=208593e7&appKey=24ff9976068fc000c241363fd8434a17&extendedOptions=useInlinedReferences"
 		    	
-		   String baseSearch = "https://api.flightstats.com/flex/schedules/rest/v1/json/from/";
-				String FlightsQuery;
-				String origincode = null;
-				String destcode = null;	
-				String year =null;
-				String month = null;
-				String day = null;
-				String appId= "appId=208593e7&";
-				String appKey="appKey=24ff9976068fc000c241363fd8434a17";
-				
-				FlightsQuery=baseSearch+origincode+"/to/"+destcode+"/arriving/"+year+"/"+month+"/"+day+"?"+appId+appKey+"&extendedOptions=useInlinedReferences";
-				}
+		/*
+		 * Building the query from specific inputs from the search page, e.g origin, destination, date.   
+		 */
+		   
+		String baseSearch = "https://api.flightstats.com/flex/schedules/rest/v1/json/from/";
+		String FlightsQuery =null;
+		String origincode = null;
+		String destcode = null;
+		String year = null;
+		String month = null;
+		String day = null;
+		String appId = "appId=208593e7&";
+		String appKey = "appKey=24ff9976068fc000c241363fd8434a17";
 
+		FlightsQuery = baseSearch + origincode + "/to/" + destcode
+				+ "/arriving/" + year + "/" + month + "/" + day + "?" + appId
+				+ appKey + "&extendedOptions=useInlinedReferences";
+		/* Now send the query and prepare the REST setup
+		 * 
+		 */
+				
+	   RestTemplate restTemplate = new RestTemplate();
+	   String JSONstring = restTemplate.getForObject("http://localhost:8080/blegh2/testflights4.json", String.class);
+	   Gson gson = new Gson();
+	   ScheduledFlights ScheduledFlights = gson.fromJson(JSONstring,ScheduledFlights.class);
+	   
+	   for (int i=0; i<ScheduledFlights.getScheduledFlights().size();i++){
+
+		    long depTime = ScheduledFlights.getScheduledFlights().get(i).getDepartureTime().getTime();
+		    long arrTime = ScheduledFlights.getScheduledFlights().get(i).getArrivalTime().getTime();
+
+		    SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+			String depInString = "13-02-2015 10:20:56";
+			String arrInString = "13-02-2015 14:20:56";
+			Boolean arrBool = false;
+			Boolean depBool = false;
+		   
+			try {
+				Date inputDep = sdf.parse(depInString);
+				Date inputArr =sdf.parse(arrInString);
+				depBool = TimeTest.getTimeInRange(depTime, inputDep, inputArr);
+				arrBool = TimeTest.getTimeInRange(arrTime, inputDep, inputArr);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	    	if (arrBool && depBool){
+		       System.out.println("Time of Departure: "+ScheduledFlights.getScheduledFlights().get(i).getDepartureFormattedTime());
+		       System.out.println("Date of Departure: "+ScheduledFlights.getScheduledFlights().get(i).getDepartureDate());
+		 	   System.out.println("Flying from : "+ScheduledFlights.getScheduledFlights().get(i).getdepartureAirport().getName());
+		 	   System.out.println("Leaving from Terminal: "+ ScheduledFlights.getScheduledFlights().get(i).getDepartureTerminal());
+		 	   System.out.println("Weather Zone: " +ScheduledFlights.getScheduledFlights().get(i).getdepartureAirport().getWeatherZone());
+		 	   System.out.println("Departure Country: "+ScheduledFlights.getScheduledFlights().get(i).getdepartureAirport().getCountryName());
+		 	   System.out.println("Country/State Code: "+ScheduledFlights.getScheduledFlights().get(i).getdepartureAirport().getStateCode());
+		 	   System.out.println("Number of Stops: "+ScheduledFlights.getScheduledFlights().get(i).getStops());
+		 	   System.out.println("Arriving at : "+ScheduledFlights.getScheduledFlights().get(i).getarrivalAirport().getName());
+		 	   System.out.println("In Country: "+ScheduledFlights.getScheduledFlights().get(i).getarrivalAirport().getCountryName());
+//		 	   System.out.println("With Country/State Code: "+ScheduledFlights.getScheduledFlights().get(i).getarrivalAirport().getStateCode());
+		 	   System.out.println("Time of Arrival: "+ScheduledFlights.getScheduledFlights().get(i).getArrivalFormattedTime());
+		 	   System.out.println("Date of Arrival: "+ScheduledFlights.getScheduledFlights().get(i).getArrivalDate());
+		 	   System.out.println("Flight Number: " +ScheduledFlights.getScheduledFlights().get(i).getFlightNumber());
+		 	   System.out.println("Flight Carrier Name: " +ScheduledFlights.getScheduledFlights().get(i).getCarrier().getName());
+//		 	   System.out.println("Flight Carrier Phone Number: " +ScheduledFlights.getScheduledFlights().get(i).getCarrier().getPhoneNumber());
+		 	   System.out.println("Journey time : "+ScheduledFlights.getScheduledFlights().get(i).getJourneyTime());
+		 	   System.out.println("Arriving at Terminal: "+ ScheduledFlights.getScheduledFlights().get(i).getArrivalTerminal());
+	    	}
+	    	else {
+	    		continue;
+	    	}
+	    }
+	}	
 }
--- GetYourWay/src/main/java/com/qa/web/Flights.java
+++ GetYourWay/src/main/java/com/qa/web/Flights.java
@@ -3,15 +3,13 @@ package com.qa.web;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Flights {
-	
+
 	private String flightNumber;
 	private int stops;
 	private String departureTerminal;
@@ -19,63 +17,116 @@ public class Flights {
 	private String departureTime;
 	private String arrivalTime;
 	private Carrier carrier = new Carrier();
-	private DepartureAirport departureAirport= new DepartureAirport();
+	private DepartureAirport departureAirport = new DepartureAirport();
 	private ArrivalAirport arrivalAirport = new ArrivalAirport();
 	private String journeyTime;
 
 	public String getFlightNumber() {
 		return flightNumber;
 	}
+
 	public void setFlightNumber(String flightNumber) {
 		this.flightNumber = flightNumber;
 	}
+
 	public int getStops() {
 		return stops;
 	}
+
 	public void setStops(int stops) {
 		this.stops = stops;
 	}
+
 	public String getDepartureTerminal() {
 		return departureTerminal;
 	}
+
 	public void setDepartureTerminal(String departureTerminal) {
 		this.departureTerminal = departureTerminal;
 	}
+
 	public String getArrivalTerminal() {
 		return arrivalTerminal;
 	}
+
 	public void setArrivalTerminal(String arrivalTerminal) {
 		this.arrivalTerminal = arrivalTerminal;
 	}
-	public String getDepartureTime() {
+
+	public Date getDepartureTime() {
+
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-	    Date newstring= null;
+		Date newstring = null;
 		try {
 			newstring = format.parse(departureTime);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String date = new SimpleDateFormat("dd-MM-yyyy, HH:MM z").format(newstring);
-	
-		return date;
+
+		return newstring;
 	}
+
 	public void setDepartureTime(String departureTime) {
 		this.departureTime = departureTime;
 	}
-	public String getArrivalTime() {
+	
+	/* Use this method to return a simple string for the date
+	 * rather than the complicated annoying date object in 'getDepartureTime'
+	 */
+	
+	public String getArrivalFormattedTime() {
+		long val = getArrivalTime().getTime();
+		Date date = new Date(val);
+		SimpleDateFormat df2 = new SimpleDateFormat("HH:mm");
+		String dateText = df2.format(date);
+		return dateText;
+	}
+	
+	public String getDepartureFormattedTime() {
+		long val = getDepartureTime().getTime();
+		Date date = new Date(val);
+		SimpleDateFormat df2 = new SimpleDateFormat("HH:mm");
+		String dateText = df2.format(date);
+		return dateText;
+	}
+	
+	
+	/* Use this method to return a simple string for the date
+	 * rather than the complicated annoying date object in 'getDepartureTime'
+	 */
+	
+	public String getArrivalDate() {
+		long val = getArrivalTime().getTime();
+		Date date = new Date(val);
+		SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yyyy");
+		String dateText = df2.format(date);
+		return dateText;
+	}
+	/* Use this method to return a simple string for the date
+	 * rather than the complicated annoying date object in 'getDepartureTime'
+	 */
+	
+	public String getDepartureDate() {
+		long val = getDepartureTime().getTime();
+		Date date = new Date(val);
+		SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yyyy");
+		String dateText = df2.format(date);
+		return dateText;
+	}
+
+	public Date getArrivalTime() {
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-	    Date newstring= null;
+		Date newdate = null;
 		try {
-			newstring = format.parse(arrivalTime);
+			newdate = format.parse(arrivalTime);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String date = new SimpleDateFormat("dd-MM-yyyy, HH:MM z").format(newstring);
-	
-		return date;
+		return newdate;
 	}
+
 	public void setArrivalTime(String arrivalTime) {
 		this.arrivalTime = arrivalTime;
 	}
@@ -83,57 +134,64 @@ public class Flights {
 	public Carrier getCarrier() {
 		return carrier;
 	}
+
 	public void setCarrier(Carrier carrier) {
 		this.carrier = carrier;
 	}
+
 	public DepartureAirport getdepartureAirport() {
 		return departureAirport;
 	}
+
 	public void setdepartureAirport(DepartureAirport departureAirport) {
 		this.departureAirport = departureAirport;
 	}
+
 	public ArrivalAirport getarrivalAirport() {
 		return arrivalAirport;
 	}
+
 	public void setarrivalAirport(ArrivalAirport arrivalAirport) {
 		this.arrivalAirport = arrivalAirport;
 	}
-	
-	public String getJourneyTime(){
 
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
- 
-		Date d1 = null;
-		Date d2 = null;
-		long diffSeconds =0;
-		long diffMinutes =0;
-		long diffHours =0;
+	public String getJourneyTime() {
+
+//		SimpleDateFormat format = new SimpleDateFormat(
+//				"yyyy-MM-dd'T'HH:mm:ss.SSS");
+
+		long d1 = 0;
+		long d2 = 0;
+		long diffSeconds = 0;
+		long diffMinutes = 0;
+		long diffHours = 0;
 		long diffDays = 0;
-		try {
-			d1 = format.parse(departureTime);
-			d2 = format.parse(arrivalTime);
- 
-			//in milliseconds
-			long diff = d2.getTime() - d1.getTime();
- 
-			 diffSeconds = diff / 1000 % 60;
-			 diffMinutes = diff / (60 * 1000) % 60;
-			 diffHours = diff / (60 * 60 * 1000) % 24;
-			 diffDays = diff / (24 * 60 * 60 * 1000);
- 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		if (diffDays <1) {
-			journeyTime= diffHours+" Hours and "+diffMinutes+" Minutes";
+		// try {
+		// d1 = format.parse(departureTime);
+		// d2 = format.parse(arrivalTime);
+		//
+		// //in milliseconds
+
+		d1 = getDepartureTime().getTime();
+		d2 = getArrivalTime().getTime();
+		long diff = d2 - d1;
+
+		diffSeconds = diff / 1000 % 60;
+		diffMinutes = diff / (60 * 1000) % 60;
+		diffHours = diff / (60 * 60 * 1000) % 24;
+		diffDays = diff / (24 * 60 * 60 * 1000);
+
+		if (diffDays < 1) {
+			journeyTime = diffHours + " Hours and " + diffMinutes + " Minutes";
 			return journeyTime;
 		}
-		
-		else{
-			journeyTime= ""+diffDays+" Days, "+diffHours+" Hours and "+diffMinutes+" Minutes";
+
+		else {
+			journeyTime = "" + diffDays + " Days, " + diffHours + " Hours and "
+					+ diffMinutes + " Minutes";
 			return journeyTime;
 		}
-		
+
 	}
-	
+
 }
